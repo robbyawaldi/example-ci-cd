a15d128d8350745080bbb60f1c45fc30f4ed63c9
Robby Awaldi <robbyawaldi@gmail.com>
2021-11-15 21:37:18 +0700

    deploy

--- src/migration/1636871394761-InitMigration.ts
+++ src/migration/1636871394761-InitMigration.ts
@@ -4,7 +4,7 @@ export class InitMigration1636871394761 implements MigrationInterface {
     name = 'InitMigration1636871394761'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "city" character varying NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE [IF NOT EXISTS] "user" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "city" character varying NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
