9599266e8c4c06646285eb914ba8716e1dd1df35
Robby Awaldi <robbyawaldi@gmail.com>
2021-11-15 20:17:52 +0700

    fix error testing

--- src/__test__/filterUser.test.ts
+++ src/__test__/filterUser.test.ts
@@ -1,12 +1,24 @@
 import { createConnection, getRepository } from "typeorm"
 import { User } from "../entity/User"
 import { filterByCity } from "../utils/filterUser"
+import path from 'path'
 
 describe('Filter User', () => {
 
     it('filter User By City', async () => {
 
-        await createConnection()
+        await createConnection({
+            type: "postgres",
+            host: process.env.HOST,
+            port: 5432,
+            username: "postgres",
+            password: "zxcasd",
+            database: "postgres",
+            logging: false,
+            synchronize: false,
+            migrations: [path.join(__dirname, "./migrations/*")],
+            entities: [User],
+        })
 
         const userRepository = getRepository(User)
 
